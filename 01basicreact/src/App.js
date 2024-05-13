40fd6271e2e6947b602d054f8ce5477fc67bddf2
Harshal Dhongadi <harshal.dhongadi@anmsoft.com>
2024-05-13 17:30:25 +0530

    basics of reactjs

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,16 @@
+import Youtube from "./Youtube";
+
+
+function App() {
+  return (
+    <div className="App">
+     
+        
+       <h1>Hello Everyone Learning react</h1>
+        <Youtube/>
+      
+    </div>
+  );
+}
+
+export default App;
