19b63313e06e22f4eeca42334882afa0c72061a2
Harshal Dhongadi <harshal.dhongadi@anmsoft.com>
2024-05-15 16:18:17 +0530

    Started with react-router-dom

---
+++ 06CurrencyConverter/src/components/InputBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const InputBox = (
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions=[],
+    selectedCurrency="usd",
+    amountDisabled=false,
+    currencyDesiabled=false,
+    className="",
+
+) => {
+  return (
+    <div>InputBox</div>
+  )
+}
+
+export default InputBox;
