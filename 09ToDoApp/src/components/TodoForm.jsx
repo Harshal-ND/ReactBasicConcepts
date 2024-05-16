f409cb9583e0525f40c3ef88700a7a6b1eccba84
Harshal Dhongadi <harshal.dhongadi@anmsoft.com>
2024-05-16 16:29:14 +0530

    Nothing much

--- 09ToDoApp/src/components/TodoForm.jsx
+++ 09ToDoApp/src/components/TodoForm.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useState } from 'react';
+import { useTodo} from '../contexts/ToDoContext'
 
 const TodoForm = () => {
+
+    const [todo, setTodo]=useState([]);
+
+    const {addTodo}=useTodo();
+
   return (
+    
     <div>TodoForm</div>
   )
 }
